e3a6c8f9ec72d5dc9cc64aad1c5fb39709f78bc9
AXC CORPORATION <142159611+lazycoder1239@users.noreply.github.com>
2026-04-23 12:30:56 +0530

    Add files via upload

---
+++ VIDEO 2 RESOURCES/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Clean;
+using Client;
+
+
+namespace Client
+{
+    internal static class Program
+    {
+
+
+
+        [UnmanagedCallersOnly(EntryPoint = "Load")]
+        public static void Load(nint pVM)
+        {
+            if (pVM != 0)
+            {
+                InternalMemory.Initialize(pVM);
+
+                var process = Process.GetCurrentProcess();
+                ComWrappers.RegisterForMarshalling(WinFormsComInterop.WinFormsComWrappers.Instance);
+                Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+                Application.EnableVisualStyles();
+                Application.Run(new Form2(process.MainWindowHandle));
+
+            }
+            else
+            {
+                MessageBox.Show("Please Restart Your Emulator And Try again.");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
